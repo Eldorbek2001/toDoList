149dc67911e4c0a109b2d69b47ead2f6a8f480b9
Eldorbek2001 <eldorbek2001@email.arizona.edu>
2022-05-23 11:56:04 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+// JShint esversion6
+
+
+const express = require("express");
+const boduParser = require("body-parser");
+const app =  express();
+app.use(boduParser.urlencoded({extended : true}))
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+var todoList = [];
+
+app.get("/", function(req, res){
+  var data = new Date();
+
+  const monthNames = ["January", "February", "March",
+                      "April", "May", "June",
+                      "July", "August", "September",
+                       "October", "November", "December"];
+  let month = monthNames[data.getMonth()];
+res.render('list', {
+  month : month,
+  day : data.getDate(),
+  year : data.getFullYear(),
+  todoList : todoList
+});
+})
+
+
+app.post("/", function(req, res){
+  var newItem = req.body.newTodoItem;
+  if(newItem.length>0){
+  todoList.push(newItem);
+  }
+    res.redirect("/");
+})
+
+
+app.listen(3000, function(){
+  console.log("Listening to port 3000")
+})
